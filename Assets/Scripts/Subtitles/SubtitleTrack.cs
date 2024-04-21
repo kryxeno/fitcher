bb87c48104f26269185975208ac6b9fd940a5e4d
Mr Bats <timvaningen63@gmail.com>
2024-04-21 21:56:54 +0200

    finished test cutscene

---
+++ Assets/Scripts/Subtitles/SubtitleTrack.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+using TMPro;
+
+[TrackBindingType(typeof(TextMeshProUGUI))]
+[TrackClipType(typeof(SubtitleClip))]
+public class SubtitleTrack : TrackAsset
+{
+
+    public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
+    {
+        return ScriptPlayable<SubtitleTrackMixer>.Create(graph, inputCount);
+    }
+
+}
